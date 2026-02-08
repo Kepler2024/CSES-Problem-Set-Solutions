75fa0dc1297962c171a6c73842916417a3e584d0
Harry C. Kepler <qilin2603379191@gmail.com>
2026-02-08 13:54:04 +0100

    Refactor gray code generation for improved clarity and efficiency

--- intro/13grayCode.cpp
+++ intro/13grayCode.cpp
@@ -1,33 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-ll fast(ll a, ll n) {
-  ll r = 1;
-  while (n) {
-    if (n&1) {
-      r = r * a;
-    }
-    a = a * a;
-    n = n >> 1;
-  }
-  return r;
-}
 
 vector<int> gray(int n) {
-  vector<int> res;
   if (n==1) {
-    res.push_back(0);
-    res.push_back(1);
+    return {0,1};
   } else {
     vector<int> pre = gray(n-1);
-    res.assign(pre.begin(),pre.end());
-    ll t = fast(2,n-1);
-    for (int i=t-1; i>=0; i--) {
+    vector<int> res = pre;
+    for (int i=pre.size()-1; i>=0; i--) {
       res.push_back(pre[i]|(1<<(n-1)));
     }
+    return res;
   }
-  return res;
 }
 
 void printBinary(int num, int n) {
