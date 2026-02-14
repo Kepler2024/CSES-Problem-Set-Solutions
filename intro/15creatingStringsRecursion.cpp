ecee3ab6cc0e4691d6306db385e44e2777a08bf9
Harry C. Kepler <qilin2603379191@gmail.com>
2026-02-14 12:04:36 +0100

    Implement recursive string generation and output in 15creatingStringsRecursion.cpp

--- intro/15creatingStringsRecursion.cpp
+++ intro/15creatingStringsRecursion.cpp
@@ -1,7 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string> res;
+bool used[8];
+
+
+void solve(string &s,string &r) {
+  if (r.length()==s.length()) {
+    res.push_back(r);
+    return;
+  }
+  for (size_t i=0;i<s.length();i++) {
+    if (used[i]) continue;
+    if ((i>0)&&(s[i]==s[i-1])&&(!used[i-1])) continue;
+
+    r.push_back(s[i]);
+    used[i] = true;
+    solve(s,r);
+
+    r.pop_back();
+    used[i] = false;
+  }
+}
+
 int main() {
-   
+  string s;
+  cin >> s;
+  string r;
+
+  sort(s.begin(),s.end());
+
+  solve(s,r);
+  cout << res.size() << endl;
+  for (auto i:res) cout << i << endl;
+  
   return 0;
 }
